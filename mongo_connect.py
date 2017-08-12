ea10dfeb55e30e0024d84c5f8fd5c54a050d39a0
Michael Foong <mikefoong@rawfactor.net>
2017-08-12 16:20:30 +0000

    CRUD operations with mongo

--- mongo_connect.py
+++ mongo_connect.py
@@ -15,8 +15,33 @@ mongo = PyMongo(app)
 @app.route('/add')
 def add():
     user = mongo.db.users
-    user.insert({'name' : 'Michael'})
+    user.insert({'name' : 'Michael', 'language':'python'})
+    user.insert({'name' : 'Kelly', 'language':'C'})
+    user.insert({'name' : 'John', 'language':'Java'})
+    user.insert({'name' : 'Cedric', 'language':'Haskell'})
     return 'Added User!'
 	
+@app.route('/find')
+def find():
+    user = mongo.db.users
+    cedric = user.find_one({'name':'Cedric'})
+    return 'You found ' + cedric['name'] + '. His favourite language is ' + cedric['language']
+
+@app.route('/update')
+def update():
+    user = mongo.db.users
+    john = user.find_one({'name':'John'})
+    john['language'] = 'JavaScript'
+    user.save(john)
+    return 'Updated John!!'
+
+@app.route('/delete')
+def delete():
+    user = mongo.db.users
+    kelly = user.find_one({'name':'Kelly'})
+    user.remove(kelly)
+    return 'Removed Kelly!'
+
+
 if __name__ == '__main__':
     app.run()
